38be66ed5d8ea32df47cea05342ea1baee3e0b4a
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-24 15:35:11 +0300

    addHealthyAddition methods

--- src/com/workintech/burger/HealthyBurger.java
+++ src/com/workintech/burger/HealthyBurger.java
@@ -3,10 +3,20 @@ package com.workintech.burger;
 public class HealthyBurger extends Hamburger{
     private String healthyExtra1Name;
     private double healthyExtra1Price;
-    private String healthyExtra12Name;
+    private String healthyExtra2Name;
     private double healthyExtra2Price;
 
     public HealthyBurger(String meat, String breadRollType) {
         super("Healthy Burger", meat, breadRollType);
+    }
+
+    public void addHealthyAddition1(String name, double price) {
+        healthyExtra1Name = name;
+        healthyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        healthyExtra2Name = name;
+        healthyExtra2Price = price;
     }
 }
