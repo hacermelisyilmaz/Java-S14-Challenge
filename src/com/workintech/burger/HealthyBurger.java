484c7cc496a0f454331d9be2db052d6de739f2c7
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-24 15:33:30 +0300

    Variables and constructor

--- src/com/workintech/burger/HealthyBurger.java
+++ src/com/workintech/burger/HealthyBurger.java
@@ -1,4 +1,12 @@
 package com.workintech.burger;
 
 public class HealthyBurger extends Hamburger{
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+    private String healthyExtra12Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String meat, String breadRollType) {
+        super("Healthy Burger", meat, breadRollType);
+    }
 }
