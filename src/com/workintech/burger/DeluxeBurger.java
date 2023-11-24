496c08f272020a2b58be1c74972ab1763961f560
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-24 15:28:56 +0300

    Child class 1 constructor and overridden addition methods

--- src/com/workintech/burger/DeluxeBurger.java
+++ src/com/workintech/burger/DeluxeBurger.java
@@ -1,4 +1,29 @@
 package com.workintech.burger;
 
 public class DeluxeBurger extends Hamburger{
+    public DeluxeBurger(String meat, String breadRollType) {
+        super("Deluxe Burger", meat, 19.90, breadRollType);
+        super.setAddition1Name("chips");
+        super.setAddition2Name("drink");
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("You cannot select additions.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("You cannot select additions.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("You cannot select additions.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("You cannot select additions.");
+    }
 }
