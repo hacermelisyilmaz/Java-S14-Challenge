9a1883f66ff2b5f2204ac983a35d4862bc27c180
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-24 14:50:53 +0300

    Addition variables and methods

--- src/com/workintech/burger/Hamburger.java
+++ src/com/workintech/burger/Hamburger.java
@@ -5,4 +5,34 @@ public class Hamburger {
     private String meat;
     private double price;
     private String breadRollType;
+    private String addition1Name;
+    private double addition1Price;
+    private String addition2Name;
+    private double addition2Price;
+    private String addition3Name;
+    private double addition3Price;
+    private String addition4Name;
+    private double addition4Price;
+
+    public void addHamburgerAddition1(String name, double price) {
+        addition1Name = name;
+        addition1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        addition2Name = name;
+        addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        addition3Name = name;
+        addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        addition4Name = name;
+        addition4Price = price;
+    }
+
+
 }
