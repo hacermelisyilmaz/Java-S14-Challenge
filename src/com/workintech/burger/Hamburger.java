a465d63081045123a40b31613faaf5c63d8952a2
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-24 15:22:02 +0300

    Getters and setters in parent class

--- src/com/workintech/burger/Hamburger.java
+++ src/com/workintech/burger/Hamburger.java
@@ -46,6 +46,102 @@ public class Hamburger {
         return price + addition1Price + addition2Price + addition3Price + addition4Price;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBreadRollType() {
+        return breadRollType;
+    }
+
+    public void setBreadRollType(String breadRollType) {
+        this.breadRollType = breadRollType;
+    }
+
+    public String getAddition1Name() {
+        return addition1Name;
+    }
+
+    public void setAddition1Name(String addition1Name) {
+        this.addition1Name = addition1Name;
+    }
+
+    public double getAddition1Price() {
+        return addition1Price;
+    }
+
+    public void setAddition1Price(double addition1Price) {
+        this.addition1Price = addition1Price;
+    }
+
+    public String getAddition2Name() {
+        return addition2Name;
+    }
+
+    public void setAddition2Name(String addition2Name) {
+        this.addition2Name = addition2Name;
+    }
+
+    public double getAddition2Price() {
+        return addition2Price;
+    }
+
+    public void setAddition2Price(double addition2Price) {
+        this.addition2Price = addition2Price;
+    }
+
+    public String getAddition3Name() {
+        return addition3Name;
+    }
+
+    public void setAddition3Name(String addition3Name) {
+        this.addition3Name = addition3Name;
+    }
+
+    public double getAddition3Price() {
+        return addition3Price;
+    }
+
+    public void setAddition3Price(double addition3Price) {
+        this.addition3Price = addition3Price;
+    }
+
+    public String getAddition4Name() {
+        return addition4Name;
+    }
+
+    public void setAddition4Name(String addition4Name) {
+        this.addition4Name = addition4Name;
+    }
+
+    public double getAddition4Price() {
+        return addition4Price;
+    }
+
+    public void setAddition4Price(double addition4Price) {
+        this.addition4Price = addition4Price;
+    }
+
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
